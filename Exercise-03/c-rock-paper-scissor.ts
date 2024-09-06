cd7be25380266a4f327dfadf20ae586024ce7a2b
salim45d <salim.agus@live.com>
2024-09-07 01:00:34 +0800

    feat: Rock Paper Scissor Game

--- Exercise-03/c-rock-paper-scissor.ts
+++ Exercise-03/c-rock-paper-scissor.ts
@@ -3,3 +3,37 @@
 ////////////////////////=>
 
 /*  => Example: if you throw a rock as an argument and the function pick a scissor then it will return 'Win'*/
+
+const player: string = "rock";
+
+function roshambo(word: string) {
+  const alpabhet: string = "ABCDEFGHI";
+  let opponent: string = alpabhet.charAt(
+    Math.floor(Math.random() * alpabhet.length)
+  );
+
+  if (opponent == "A" || opponent == "F" || opponent == "H") {
+    opponent = "rock";
+  } else if (opponent == "C" || opponent == "G" || opponent == "E") {
+    opponent = "paper";
+  } else {
+    opponent = "scissor";
+  }
+  console.log(opponent);
+
+  let resultRoshambo: string = "";
+  if (player == opponent) {
+    resultRoshambo = "Draw";
+  } else if (player == "rock") {
+    resultRoshambo = opponent == "paper" ? "Win" : "Lose";
+  } else if (player == "paper") {
+    resultRoshambo = opponent == "scissor" ? "Win" : "Lose";
+  } else if (player == "scissor") {
+    resultRoshambo = opponent == "rock" ? "Win" : "Lose";
+  }
+
+  return resultRoshambo;
+}
+
+let resultRoshambo = roshambo(player);
+console.log(resultRoshambo);
